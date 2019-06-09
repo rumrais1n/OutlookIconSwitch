55c20b0a4a509c33a1c0a06eec3673541fdda527
rumrais1n <rumraisin@rumraisin.net>
2019-06-09 18:05:47 +0900

    jQuery読み込むやつ書いた

--- switch.js
+++ switch.js
@@ -5,5 +5,7 @@ function importScript(URL) {
 }
 
 function switchImage() {
+	importScript("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js");
+
 	$('ms-Persona-image').children('img').attr('src', 'https://pbs.twimg.com/profile_images/1100054151692574720/Rt0-3Wmx_400x400.png');
 }
