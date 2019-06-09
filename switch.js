98b7193b8610ce566c66b37cdd5617acee458433
rumrais1n <rumraisin@rumraisin.net>
2019-06-09 18:35:39 +0900

    これを書かないとTampermonkeyでエラーになるっぽい

--- switch.js
+++ switch.js
@@ -4,8 +4,9 @@ function importScript(URL) {
 	document.body.appendChild(element);
 }
 
-function switchImage() {
-	importScript("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js");
-
-	$('.ms-Image.ms-Persona-image.image-112').children('img').attr('src', 'https://pbs.twimg.com/profile_images/877432098671403008/MGlKfUNp_400x400.png');
-}
+importScript("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js");
+var $ = window.jQuery;
+$(window).on('load',function(){
+		$('.ms-Image.ms-Persona-image.image-112').children('img').attr('src', 'https://pbs.twimg.com/profile_images/877432098671403008/MGlKfUNp_400x400.png');
+	}
+)
