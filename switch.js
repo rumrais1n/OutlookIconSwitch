ca3a6dd0d4e499f31655db437b493f37783bbfad
rumrais1n <rumraisin@rumraisin.net>
2019-06-09 18:51:39 +0900

    書き方変えた

--- switch.js
+++ switch.js
@@ -6,7 +6,10 @@ function importScript(URL) {
 
 importScript("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js");
 var $ = window.jQuery;
-$(window).on('load',function(){
-		$('.ms-Image.ms-Persona-image.image-112').children('img').attr('src', 'https://pbs.twimg.com/profile_images/877432098671403008/MGlKfUNp_400x400.png');
-	}
-)
+
+function switchImage(){
+	var elements = document.getElementsByClassName('ms-Image.ms-Persona-image.image-112');
+	
+	elements.removeAttribute('src');
+	elements.setAttribute('src', 'https://pbs.twimg.com/profile_images/877432098671403008/MGlKfUNp_400x400.png');
+}
