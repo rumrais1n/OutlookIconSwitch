3eab57639c0e88efbf7e8e3fb7c73857d0081872
rumrais1n <rumraisin@rumraisin.net>
2019-06-09 18:04:12 +0900

    画像切替えるスクリプト追加

--- switch.js
+++ switch.js
@@ -1,5 +1,9 @@
 function importScript(URL) {
-	var element = document.createElement( 'script' );
+	var element = document.createElement('script');
 	element.src = URL;
-	document.body.appendChild( element );
+	document.body.appendChild(element);
+}
+
+function switchImage() {
+	$('ms-Persona-image').children('img').attr('src', 'https://pbs.twimg.com/profile_images/1100054151692574720/Rt0-3Wmx_400x400.png');
 }
